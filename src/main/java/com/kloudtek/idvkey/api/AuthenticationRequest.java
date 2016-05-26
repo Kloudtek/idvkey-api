cdee1d4f2a428cbbea20431430fcd7e9b657c2bd
Yannick Menager <ymenager@kloudtek.com>
2016-05-25 22:17:21 -0700

    Add Security level to authentication

--- src/main/java/com/kloudtek/idvkey/api/AuthenticationRequest.java
+++ src/main/java/com/kloudtek/idvkey/api/AuthenticationRequest.java
@@ -4,16 +4,34 @@
 
 package com.kloudtek.idvkey.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.net.URL;
 
 /**
  * Created by yannick on 22/3/16.
  */
 public class AuthenticationRequest extends AbstractNotificationRequest {
+    @JsonProperty
+    private SecurityLevel securityLevel;
+
     public AuthenticationRequest() {
     }
 
     public AuthenticationRequest(String serviceId, URL callbackURL, URL cancelUrl) {
         super(serviceId, callbackURL, cancelUrl);
+    }
+
+    public AuthenticationRequest(String serviceId, URL callbackUrl, URL cancelUrl, SecurityLevel securityLevel) {
+        super(serviceId, callbackUrl, cancelUrl);
+        this.securityLevel = securityLevel;
+    }
+
+    public SecurityLevel getSecurityLevel() {
+        return securityLevel;
+    }
+
+    public void setSecurityLevel(SecurityLevel securityLevel) {
+        this.securityLevel = securityLevel;
     }
 }
